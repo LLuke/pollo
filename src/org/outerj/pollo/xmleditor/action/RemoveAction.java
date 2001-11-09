27aa4cb4422e70c4e6ddbc62d617dfaee19b9a33
bdumon <bdumon>
2001-11-09 16:45:47 +0000

    select next node after deleting node

--- src/org/outerj/pollo/xmleditor/action/RemoveAction.java
+++ src/org/outerj/pollo/xmleditor/action/RemoveAction.java
@@ -2,6 +2,7 @@ package org.outerj.pollo.xmleditor.action;
 
 import org.outerj.pollo.xmleditor.XmlEditor;
 import org.outerj.pollo.xmleditor.SelectionListener;
+import org.outerj.pollo.xmleditor.view.View;
 
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
@@ -22,9 +23,21 @@ public class RemoveAction extends AbstractAction implements SelectionListener
 
 	public void actionPerformed(ActionEvent e)
 	{
+		// delete the selected node and select the previous node
 		Node node = xmlEditor.getSelectionInfo().getSelectedNode();
-		Element parent = (Element)node.getParentNode();
-		parent.removeChild(node);
+
+		// it is not allowed to delete the (visible) root node
+		if (node != xmlEditor.getRootView().getNode())
+		{
+			View newSelectedView = xmlEditor.getSelectionInfo().getSelectedNodeView().getNextButNotChild();
+			Element parent = (Element)node.getParentNode();
+			parent.removeChild(node);
+
+			int startV = newSelectedView.getVerticalPosition();
+			int startH = newSelectedView.getHorizontalPosition();
+			newSelectedView.markAsSelected(startH, startV);
+			xmlEditor.scrollAlignTop(startV, newSelectedView.getHeight());
+		}
 	}
 
 	public void nodeUnselected(Node node)
