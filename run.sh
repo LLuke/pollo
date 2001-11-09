9f7bd15993c1504924e2c06bf5a443114d836a5d
bdumon <bdumon>
2001-11-09 17:01:26 +0000

    updated classpath

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-export CLASSPATH=lib/xerces.jar:lib/xalan.jar:lib/kunststoff.jar:build
+export CLASSPATH=lib/xerces.jar:lib/kunststoff.jar:lib/commons-digester.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/log4j-core.jar:conf:build:lib/jaxen-core.jar:lib/jaxen-dom.jar:lib/saxpath.jar:.
 java org.outerj.pollo.Pollo
