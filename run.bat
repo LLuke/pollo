a0733c4657080a2a33a5416089f2f2c28d0e8524
bdumon <bdumon>
2002-01-27 18:29:52 +0000

    *** empty log message ***

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=lib\xerces.jar;lib\kunststoff.jar;lib\commons-digester.jar;lib\commons-beanutils.jar;lib\commons-collections.jar;lib\log4j-core.jar;conf;build;lib\jaxen-core.jar;lib\jaxen-dom.jar;lib\saxpath.jar;.
+set CLASSPATH=lib\xerces.jar;lib\kunststoff.jar;lib\commons-digester.jar;lib\commons-beanutils.jar;lib\commons-collections.jar;lib\log4j-core.jar;conf;build;lib\jaxen-core.jar;lib\jaxen-dom.jar;lib\saxpath.jar;lib\msv.jar;lib\xsdlib.jar;lib\relaxngDatatype.jar;lib\isorelax.jar;lib\jhbasic.jar;.
 java org.outerj.pollo.Pollo
