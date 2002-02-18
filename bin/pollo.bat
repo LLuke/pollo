ff0e4259d27fbc62725f7751b24af16e8387228e
bdumon <bdumon>
2002-02-18 20:00:47 +0000

    *** empty log message ***

---
+++ bin/pollo.bat
@@ -0,0 +1,3 @@
+set CLASSPATH=..\lib\pollo.jar;..\lib\xerces.jar;..\lib\commons-digester.jar;..\lib\commons-beanutils.jar;..\lib\commons-collections.jar;..\lib\log4j-core.jar;..\lib\jaxen-core.jar;..\lib\jaxen-dom.jar;..\lib\saxpath.jar;..\lib\msv.jar;..\lib\xsdlib.jar;..\lib\relaxngDatatype.jar;..\lib\isorelax.jar;..\lib\jhbasic.jar;..\conf;..\build
+
+java -Djava.endorsed.dirs=..\lib\endorsed org.outerj.pollo.Pollo
