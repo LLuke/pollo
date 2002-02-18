313ede770dfc1b71f54139b8663e3ba2572d78f3
bdumon <bdumon>
2002-02-18 19:55:02 +0000

    moved to bin directory

--- run.sh
+++
@@ -1,2 +0,0 @@
-export CLASSPATH=lib/xerces.jar:lib/kunststoff.jar:lib/commons-digester.jar:lib/commons-beanutils.jar:lib/commons-collections.jar:lib/log4j-core.jar:conf:build:lib/jaxen-core.jar:lib/jaxen-dom.jar:lib/saxpath.jar:lib/msv.jar:lib/xsdlib.jar:lib/relaxngDatatype.jar:lib/isorelax.jar:.
-java org.outerj.pollo.Pollo
