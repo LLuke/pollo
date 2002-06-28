d4c664421e8e6de367725370d9ea0a11780806f5
bdumon <bdumon>
2002-06-28 09:13:34 +0000

    Added 'res' directory to classpath

--- bin/pollo.bat
+++ bin/pollo.bat
@@ -1,3 +1,3 @@
-set CLASSPATH=..\lib\pollo.jar;..\lib\pollohelp.jar;..\lib\endorsed\xerces.jar;..\lib\commons-digester.jar;..\lib\commons-beanutils.jar;..\lib\commons-collections.jar;..\lib\log4j-core.jar;..\lib\jaxen-core.jar;..\lib\jaxen-dom.jar;..\lib\saxpath.jar;..\lib\msv.jar;..\lib\xsdlib.jar;..\lib\relaxngDatatype.jar;..\lib\isorelax.jar;..\lib\jhbasic.jar;..\conf;..\build
+set CLASSPATH=..\lib\pollo.jar;..\lib\pollohelp.jar;..\lib\endorsed\xerces.jar;..\lib\commons-digester.jar;..\lib\commons-beanutils.jar;..\lib\commons-collections.jar;..\lib\log4j-core.jar;..\lib\jaxen-core.jar;..\lib\jaxen-dom.jar;..\lib\saxpath.jar;..\lib\msv.jar;..\lib\xsdlib.jar;..\lib\relaxngDatatype.jar;..\lib\isorelax.jar;..\lib\jhbasic.jar;..\conf;..\res;..\build
 
 java -Djava.endorsed.dirs=..\lib\endorsed org.outerj.pollo.Pollo
--- bin/pollo.sh
+++ bin/pollo.sh
@@ -1,3 +1,3 @@
-export CLASSPATH=../lib/pollo.jar:../lib/pollohelp.jar:../lib/endorsed/xerces.jar:../lib/jhbasic.jar:../lib/commons-digester.jar:../lib/commons-beanutils.jar:../lib/commons-collections.jar:../lib/log4j-core.jar:../lib/jaxen-core.jar:../lib/jaxen-dom.jar:../lib/saxpath.jar:../lib/msv.jar:../lib/xsdlib.jar:../lib/relaxngDatatype.jar:../lib/isorelax.jar:../conf:../build
+export CLASSPATH=../lib/pollo.jar:../lib/pollohelp.jar:../lib/endorsed/xerces.jar:../lib/jhbasic.jar:../lib/commons-digester.jar:../lib/commons-beanutils.jar:../lib/commons-collections.jar:../lib/log4j-core.jar:../lib/jaxen-core.jar:../lib/jaxen-dom.jar:../lib/saxpath.jar:../lib/msv.jar:../lib/xsdlib.jar:../lib/relaxngDatatype.jar:../lib/isorelax.jar:../conf:../build:../res
 
 java -Djava.endorsed.dirs=../lib/endorsed org.outerj.pollo.Pollo
