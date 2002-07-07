ab675225aaa643bfe80cc0cbc2f41803c7997ad6
bdumon <bdumon>
2002-07-07 15:35:06 +0000

    Added disposing of attribute panel

--- src/org/outerj/pollo/xmleditor/XmlEditorPanel.java
+++ src/org/outerj/pollo/xmleditor/XmlEditorPanel.java
@@ -263,6 +263,7 @@ public class XmlEditorPanel extends JPanel implements DomConnected, Disposable
 	public void dispose()
 	{
 		queryByXPathPanel.dispose();
+		attrPanel.dispose();
 		remove(xmlEditor);
 	}
 }
