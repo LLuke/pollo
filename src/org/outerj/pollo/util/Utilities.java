0c415680c637174545a83a08483ef8bf8fa05a76
bdumon <bdumon>
2002-07-10 10:22:09 +0000

    initial commit

---
+++ src/org/outerj/pollo/util/Utilities.java
@@ -0,0 +1,20 @@
+package org.outerj.pollo.util;
+
+import javax.swing.*;
+
+public class Utilities
+{
+	/**
+	 * Creates a JMenuItem based on an Action. The Swing implementation
+	 * in JDK 1.3 will not copy the accelerator from the action to the
+	 * JMenuItem, therefore this method will do this additionally.
+	 */
+	public static JMenuItem createMenuItemFromAction(Action action)
+	{
+		JMenuItem menuItem = new JMenuItem();
+		menuItem.setAction(action);
+		menuItem.setAccelerator((KeyStroke)action.getValue(Action.ACCELERATOR_KEY));
+
+		return menuItem;
+	}
+}
