ff3d18d7f196c218e9be951eb94379cd8046367b
bdumon <bdumon>
2002-08-17 12:07:30 +0000

    Adjustments for the new xerces jar's.

--- bin/pollo.sh
+++ bin/pollo.sh
@@ -1,3 +1,3 @@
-export CLASSPATH=../lib/pollo.jar:../lib/pollohelp.jar:../lib/endorsed/xerces.jar:../lib/jhbasic.jar:../lib/commons-digester.jar:../lib/commons-beanutils.jar:../lib/commons-collections.jar:../lib/log4j-core.jar:../lib/jaxen-core.jar:../lib/jaxen-dom.jar:../lib/saxpath.jar:../lib/msv.jar:../lib/xsdlib.jar:../lib/relaxngDatatype.jar:../lib/isorelax.jar:../conf:../build:../res
+export CLASSPATH=../lib/pollo.jar:../lib/pollohelp.jar:../lib/endorsed/dom3-xercesImpl.jar:../lib/endorsed/dom3-xmlParserAPIs.jar:../lib/jhbasic.jar:../lib/commons-digester.jar:../lib/commons-beanutils.jar:../lib/commons-collections.jar:../lib/log4j-core.jar:../lib/jaxen-core.jar:../lib/jaxen-dom.jar:../lib/saxpath.jar:../lib/msv.jar:../lib/xsdlib.jar:../lib/relaxngDatatype.jar:../lib/isorelax.jar:../conf:../build:../res
 
 java -Djava.endorsed.dirs=../lib/endorsed org.outerj.pollo.Pollo $@
