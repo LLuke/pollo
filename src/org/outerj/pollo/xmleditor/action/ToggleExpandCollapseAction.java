83618b56c443eef589055e5b67f73304f788ef47
bdumon <bdumon>
2002-09-06 07:32:17 +0000

    Removed focus check, not needed anymore since shortcut is now changed to control+space instead of just space

--- src/org/outerj/pollo/xmleditor/action/ToggleExpandCollapseAction.java
+++ src/org/outerj/pollo/xmleditor/action/ToggleExpandCollapseAction.java
@@ -19,11 +19,6 @@ public class ToggleExpandCollapseAction extends AbstractNodeAction
 
     public void actionPerformed(ActionEvent e)
     {
-        // Note: this action is normally bound to the space key. This gave problems combined with
-        // the CharDataPanel: when pressing space in the textarea over there, this action was also
-        // executed, meaning that the event was apparently not consumed there.
-        if (!xmlEditor.hasFocus())
-            return;
         View view = xmlEditor.getSelectionInfo().getSelectedNodeView();
         if (view.getNode().getNodeType() != Node.DOCUMENT_NODE)
         {
