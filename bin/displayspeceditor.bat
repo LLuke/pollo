9c2496c833d0bc10bbdb52034d234d4dda4c3bc1
bdumon <bdumon>
2003-09-12 15:18:12 +0000

    removed display spec editor

--- bin/displayspeceditor.bat
+++
@@ -1,1 +0,0 @@
-java -classpath ..\lib\pollo.jar;..\lib\endorsed\dom3-xmlParserAPIs.jar;..\lib\endorsed\dom3-xercesImpl.jar;..\build -Djava.endorsed.dirs=..\lib\endorsed org.outerj.pollo.displayspeceditor.DisplaySpecificationEditor %1
--- bin/displayspeceditor.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=../lib/pollo.jar:../lib/endorsed/dom3-xercesImpl.jar:../lib/endorsed/dom3-xmlParserAPIs.jar:../build
-
-java -Djava.endorsed.dirs=../lib/endorsed org.outerj.pollo.displayspeceditor.DisplaySpecificationEditor $@
