623f68aa9c8afb135ad89ea07a88994383256137
bdumon <bdumon>
2003-09-12 14:50:55 +0000

    removed display spec editor experiment. There's now a viewtypeconf for editing display specs.

--- src/org/outerj/pollo/displayspeceditor/model/InvalidQNameException.java
+++
@@ -1,9 +0,0 @@
-package org.outerj.pollo.displayspeceditor.model;
-
-public class InvalidQNameException extends Exception
-{
-    public InvalidQNameException(String message)
-    {
-        super(message);
-    }
-}
